5766fa3aeed9d018e9c711a875ff9c30ed806158
JOSHCLUNE <josh.seth.clune@gmail.com>
2026-08-18 03:02:37 +0000

    GrindSuggestionsTest

--- tallySummaries.sh
+++ tallySummaries.sh
@@ -9,9 +9,13 @@ total_successes=0
 total_time=0
 
 for f in "$DIR"/*; do
-    results=$(grep -oP 'Total number of results: \K[0-9]+' "$f")
-    successes=$(grep -oP 'Total number of successes: \K[0-9]+' "$f")
-    time_taken=$(grep -oP 'Time taken: \K[0-9]+' "$f")
+    if ! grep -q 'Total number of results:' "$f"; then
+        echo "Warning: $f has no results tally (timed out?); counting as 0" >&2
+    fi
+
+    results=$(grep -oP 'Total number of results: \K[0-9]+' "$f" || echo 0)
+    successes=$(grep -oP 'Total number of successes: \K[0-9]+' "$f" || echo 0)
+    time_taken=$(grep -oP 'Time taken: \K[0-9]+' "$f" || echo 0)
 
     total_results=$((total_results + results))
     total_successes=$((total_successes + successes))
